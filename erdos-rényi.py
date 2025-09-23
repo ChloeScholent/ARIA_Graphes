bbd151487761294c520436e19cbadd347bdd6a2e
ChloeScholent <159830563+ChloeScholent@users.noreply.github.com>
2025-09-23 22:31:27 +0200

    Update erdos-rényi.py

--- erdos-rényi.py
+++ erdos-rényi.py
@@ -12,12 +12,13 @@ print("node degree clustering")
 for v in nx.nodes(G):
     print(f"{v} {nx.degree(G, v)} {nx.clustering(G, v)}")
 
-print()
 print("the adjacency list")
-for line in nx.generate_adjlist(G):
-    print(line)
+with open('erdos-renyi_edges.txt', "w") as f:
+    for line in nx.edges(G):
+        print(line)
+        f.write(f'{line}\n')
 
-pos = nx.spring_layout(G)
-nx.draw(G, pos=pos)
-plt.show()
+# pos = nx.spring_layout(G)
+# nx.draw(G, pos=pos)
+# plt.show()
 
